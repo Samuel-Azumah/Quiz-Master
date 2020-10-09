1754209bad5d238fe4ff5404c67ddf0c7beb22e3
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-08 23:04:27 -1200

    Update play.js

--- play.js
+++ play.js
@@ -56,3 +56,22 @@ startGame = () => {
     game.classList.remove('hidden');
     loader.classList.add('hidden');
 };
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerHTML = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerHTML = currentQuestion['choice' + number];
+    });
