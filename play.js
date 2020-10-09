73ca031ac4b37df30ae3f0abd402a6fb488cf4df
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-08 23:18:17 -1200

    Update play.js

--- play.js
+++ play.js
@@ -40,10 +40,8 @@ fetch(
     .catch((err) => {
         console.error(err);
     });
-
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
-
 startGame = () => {
     questionCounter = 0;
     score = 0;
@@ -55,18 +53,15 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        //go to the end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerHTML = currentQuestion.question;
-
     choices.forEach((choice) => {
         const number = choice.dataset['number'];
         choice.innerHTML = currentQuestion['choice' + number];
@@ -74,22 +69,18 @@ getNewQuestion = () => {
 availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
-
 choices.forEach((choice) => {
     choice.addEventListener('click', (e) => {
         if (!acceptingAnswers) return;
-
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
 
         const classToApply =
             selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
         if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
-
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
