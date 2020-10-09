6cb83aa71b78dc19b605ac3af33a00fa18ad7e6f
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-08 23:19:16 -1200

    Update play.js

--- play.js
+++ play.js
@@ -75,21 +75,18 @@ choices.forEach((choice) => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-
         const classToApply =
             selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
         if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
         selectedChoice.parentElement.classList.add(classToApply);
-
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
     });
 });
-
 incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
