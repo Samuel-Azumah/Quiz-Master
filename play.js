af1554749394a95dd94b4b57890d1d3c8e1a8b70
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-08 22:39:03 -1200

    Update play.js

--- play.js
+++ play.js
@@ -2,3 +2,7 @@ const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+let currentQuestion = {};
