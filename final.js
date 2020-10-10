8de2d851706c7eb2d1f3fd9743e1d141d72d436d
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-09 23:33:59 -1200

    Update final.js

--- final.js
+++ final.js
@@ -14,3 +14,10 @@ saveHighScore = (e) => {
         score: mostRecentScore,
         name: username.value,
     };
+        highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+}
